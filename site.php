250ac78992cc4d6f9ca3df23958d0ec0453985dc
LotusMel <46916651+LotusMel@users.noreply.github.com>
2020-05-24 01:25:58 -0500

    Add files via upload
    Added basic Welcome function

---
+++ site.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Melie's Site</title>
+  </head>
+  <body>
+
+    <?php
+      function Welcome(){
+        echo "<h1>Hello. Welcome to my Project Site";
+      }
+      Welcome();
+    ?>
+
+  </body>
+</html>
